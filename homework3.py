fed96ea24d34c179c873c60421644fcdec0cec14
Ильдус <bif1981@yandex.ru>
2024-04-15 16:12:03 +0400

    Поправил расположение программы в файлах.

---
+++ homework3.py
@@ -0,0 +1,13 @@
+my_string = str(input('Введите произвольный текст: '))
+
+print(len(my_string))   # Вывести длину введенной строки
+
+print(my_string.upper())    # Выводим строку в верхнем регистре
+
+print(my_string.lower())    # Выводим строку в нижнем регистре
+
+print(my_string.replace(' ', '')) # Удаление всех пробелов
+
+print(my_string[0])     # Вывожу первый символ строки my_string
+
+print(my_string[-1])    # Вывожу последний символ строки my_string
