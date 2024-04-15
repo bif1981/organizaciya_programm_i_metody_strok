3052fa8f1c1f9e7a7172673d11034e070e06cf5a
Ильдус <bif1981@yandex.ru>
2024-04-15 16:06:00 +0400

    Практическая работа по уроку "Организация программ и методы строк."

---
+++ main.py
@@ -0,0 +1,18 @@
+my_string = str(input('Введите произвольный текст: '))
+
+print(len(my_string))   # Вывести длину введенной строки
+
+print(my_string.upper())    # Выводим строку в верхнем регистре
+
+print(my_string.lower())    # Выводим строку в нижнем регистре
+
+print(my_string.replace(' ', '')) # Удаление всех пробелов
+
+print(my_string[0])
+print(my_string[-1])
+
+
+
+# Измените строку my_string, удалив все пробелы.
+# Выведите первый символ строки my_string.
+# Выведите последний символ строки my_string.
